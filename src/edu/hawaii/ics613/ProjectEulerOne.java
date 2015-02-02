3b9ae7fcd08f9754987a2758439e78b76699107f
Branden Ogata <branden.s.ogata@gmail.com>
2015-02-01 14:40:40 -1000

    Moves Java file into package and adds documentation for the file.

--- src/edu/hawaii/ics613/ProjectEulerOne.java
+++ src/edu/hawaii/ics613/ProjectEulerOne.java
@@ -1,6 +1,12 @@
+package edu.hawaii.ics613;
+
 /**
+ * Prints the sum of all multiples of 3 or 5 less than 1000.
+ *
  * Created by Branden Ogata on 2/1/2015.
+ *
  */
+
 public class ProjectEulerOne {
   public static void main(String[] args) {
     int total = 0;
